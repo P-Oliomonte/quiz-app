a4b93454deafc18f7c4d86f34ad93cab7920cdb4
Phillip Oehlenberg <hi@phillip-oehlenberg.com>
2024-05-03 12:11:16 +0200

    Update functionality of form creating

--- js/form.js
+++ js/form.js
@@ -17,13 +17,24 @@ inputQuestion.focus();
 
 function characterCounter(input, counter) {
   input.addEventListener("input", () => {
-    charactersLeft = 150 - input.value.length;
+    charactersLeft = input.getAttribute("maxlength") - input.value.length;
     counter.textContent = charactersLeft;
   });
 }
 
 characterCounter(inputQuestion, characterCounterQuestion);
 characterCounter(inputAnswer, characterCounterAnswer);
+
+// Function to add '#'
+
+function hashtag(data) {
+  const input = data.tag.split(" ").join("");
+  if (input.startsWith("#")) {
+    return input;
+  } else {
+    return "#" + input;
+  }
+}
 
 // Here starts the function for form submission and card creation/
 
@@ -33,70 +44,89 @@ form.addEventListener("submit", (event) => {
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
 
-  console.log(data);
+  // Declaring input variables
 
-  event.target.question.focus();
+  const questionInput = data.question;
+  const answerInput = data.answer;
+  const tagInput = hashtag(data);
+  //   let tagInput = data.tag.split(" ").join("");
 
-  const question = data.question;
-  const answer = data.answer;
-  const tag = data.tag.split(" ").join("");
+  //   tagInput = hashtag(tagInput);
+  // Creating elements and appending them to the DOM
 
-  const newQuestionCard = document.createElement("section");
-  newQuestionCard.classList.add("question-card");
+  const questionCard = document.createElement("section");
+  questionCard.classList.add("question-card");
 
-  newQuestionCard.innerHTML = `<div class="bookmark-container">
-  <svg
-    class="bookmark"
-    data-js="bookmark"
-    data-name="Ebene 1"
-    xmlns="http://www.w3.org/2000/svg"
-    version="1.1"
-    viewBox="0 0 200 200"
-  >
-    <path class="cls-1" d="M197,3H3v191l97-55,97,55V3h0Z" />
-  </svg>
-</div>
-<h2 class="question">${question}</h2>
+  const bookmarkContainer = document.createElement("div");
+  bookmarkContainer.classList.add("bookmark-container");
+  bookmarkContainer.innerHTML = `<svg
+            class="bookmark"
+            data-js="bookmark"
+            data-name="Ebene 1"
+            xmlns="http://www.w3.org/2000/svg"
+            version="1.1"
+            viewBox="0 0 200 200"
+          >
+            <path class="cls-1" d="M197,3H3v191l97-55,97,55V3h0Z" />
+          </svg>`;
 
-<button class="button" data-js="button-answer1">
-  Show answer<i class="bi bi-caret-down-fill button-icon"></i>
-</button>
+  const svg = bookmarkContainer.querySelector("svg");
 
-<p class="answer answer-hide" data-js="answer1">
-  ${answer}
-</p>
-
-<div class="tags-container">
-  <a class="tag" href="#">#${tag}</a>
-</div>`;
-
-  mainContainer.append(newQuestionCard);
-
-  event.target.reset();
-
-  // Here starts the bookmark and answer toggle functionality
-
-  const bookmarks = document.querySelectorAll("[data-js='bookmark']");
-
-  bookmarks.forEach((bookmark) => {
-    // Add click event listener to each bookmark
-    bookmark.addEventListener("click", () => {
-      // Toggle the 'bookmark-checked' class on the clicked bookmark
-      bookmark.classList.toggle("bookmark-checked");
-    });
+  // Add click event listener to each bookmark
+  svg.addEventListener("click", () => {
+    // Toggle the 'bookmark-checked' class on the clicked bookmark
+    svg.classList.toggle("bookmark-checked");
   });
 
-  const buttonAnswer1 = document.querySelector('[data-js="button-answer1"]');
-  const answer1 = document.querySelector('[data-js="answer1"]');
+  questionCard.append(bookmarkContainer);
 
-  buttonAnswer1.addEventListener("click", () => {
-    answer1.classList.toggle("answer-hide");
+  const question = document.createElement("h2");
+  question.classList.add("question");
+  question.textContent = questionInput;
 
-    if (answer1.classList.contains("answer-hide")) {
-      buttonAnswer1.innerHTML =
+  questionCard.append(question);
+
+  const hideShowButton = document.createElement("button");
+  hideShowButton.classList.add("button");
+  hideShowButton.setAttribute("data-js", "button-answer1");
+  hideShowButton.innerHTML = `Show answer<i class="bi bi-caret-down-fill button-icon"></i>`;
+
+  questionCard.append(hideShowButton);
+
+  const answer = document.createElement("p");
+  answer.classList.add("answer", "answer-hide");
+  answer.setAttribute("data-js", "answer");
+  answer.textContent = answerInput;
+
+  questionCard.append(answer);
+
+  const tagContainer = document.createElement("div");
+  tagContainer.classList.add("tags-container");
+
+  const tag = document.createElement("a");
+  tag.classList.add("tag");
+  tag.setAttribute("href", "#");
+  tag.textContent = tagInput;
+
+  tagContainer.append(tag);
+
+  questionCard.append(tagContainer);
+
+  mainContainer.append(questionCard);
+
+  event.target.reset();
+  event.target.question.focus();
+
+  // Here is the event li
+
+  hideShowButton.addEventListener("click", () => {
+    answer.classList.toggle("answer-hide");
+
+    if (answer.classList.contains("answer-hide")) {
+      hideShowButton.innerHTML =
         'Show answer<i class="bi bi-caret-down-fill button-icon"></i>';
     } else {
-      buttonAnswer1.innerHTML =
+      hideShowButton.innerHTML =
         'Hide answer<i class="bi bi-caret-up-fill button-icon"></i>';
     }
   });
